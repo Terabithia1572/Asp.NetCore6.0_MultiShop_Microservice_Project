4bf028bce1bebdd43cf5562134387dc212f9bebb
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-23 21:46:16 +0300

    8# ProductImage & ProductDetail Entities Process Added

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -2,5 +2,8 @@
 {
     public class ProductDetail
     {
+        public string ProductDetailID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
+        public string ProductDetailDescription { get; set; } // Ürün Detay Açıklamasını Tuttuk.
+        public string ProductDetailInfo { get; set; } //Ürün Detay Bilgilerini Tuttuk.
     }
 }
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductImages
+    {
+        public string ProductImagesID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
+        public string ProductImage1 { get; set; } //Birden fazla ürünün resmi varsa yolunu birden fazla tabloda tutacağız.
+        public string ProductImage2 { get; set; } //İkinci resim için
+        public string ProductImage3 { get; set; } //Üçüncü resim için
+        public string ProductID { get; set; } //Ürün ile ilişkili hale getirmek için ProductID ekledik.
+        public Product Product { get; set; } //Product tablosuyla ilişki ekledik.
+
+    }
+}
