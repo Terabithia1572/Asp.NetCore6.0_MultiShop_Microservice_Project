e752f48cff568af3d44d03ac6a44aaf4ce457590
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-23 22:23:31 +0300

    12# ProductDetail DTO Process Added

---
+++ Services/Catalog/MultiShop.Catalog/DTOs/ProductDetailDTOs/CreateProductDetailDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Catalog.DTOs.ProductDetailDTOs
+{
+    public class CreateProductDetailDTO
+    {
+        public string ProductDetailDescription { get; set; } // Ürün Detay Açıklamasını Tuttuk.
+        public string ProductDetailInfo { get; set; } //Ürün Detay Bilgilerini Tuttuk.
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/DTOs/ProductDetailDTOs/ResultProductDetailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.DTOs.ProductDetailDTOs
+{
+    public class ResultProductDetailDTO
+    {
+        public string ProductDetailID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
+        public string ProductDetailDescription { get; set; } // Ürün Detay Açıklamasını Tuttuk.
+        public string ProductDetailInfo { get; set; } //Ürün Detay Bilgilerini Tuttuk.
+    }
+}
