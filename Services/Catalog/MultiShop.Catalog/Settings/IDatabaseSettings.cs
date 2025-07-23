7c78b91e83738b18d0320c233092b9d0b62db5a6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-23 22:00:25 +0300

    10# Settings Interface & Class Process Added

---
+++ Services/Catalog/MultiShop.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string CategoryCollectionName { get; set; } // Kategori koleksiyonunun adını tutar.
+        public string ProductCollectionName { get; set; } // Ürün koleksiyonunun adını tutar.
+        public string ProductDetailCollectionName { get; set; } // Ürün detay koleksiyonunun adını tutar.
+        public string ProductImageCollectionName { get; set; } // Ürün resim koleksiyonunun adını tutar.
+        public string ConnectionString { get; set; } // MongoDB bağlantı dizesini tutar.
+        public string DatabaseName { get; set; } // Veritabanı adını tutar.
+    }
+}
