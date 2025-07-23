366dc3ba1f7ce4100a1e7951adfb86a225c20e7a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-23 21:38:35 +0300

    7# Product & Category Entities Process Added

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class Category
     {
+        [BsonId] //Bu attribute, hangi property’nin MongoDB’de “_id” alanı olarak tutulacağını belirtir.
+
+        [BsonRepresentation(BsonType.ObjectId)] // Bu attribute, MongoDB’deki ObjectId türündeki verilerin string olarak temsil edilmesini sağlar.
+        public string CategoryID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
+        public string CategoryName { get; set; } //Kategori Adını Tuttuk.
+
     }
 }
---
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
+        public string ProductName { get; set; } // Ürün Adını Tuttuk.
+        public decimal ProductPrice { get; set; } //Ürün Fiyatını Tuttuk.
+        public string ProductDescription { get; set; } //Ürün Açıklamasını Tuttuk.
+        public string ProductImageURL { get; set; } //Ürün Resmini Tuttuk.
+        public string CategoryID { get; set; } // Ürünün ait olduğu kategori ID'sini tuttuk.
+        [BsonIgnore] // Bu attribute, MongoDB'de bu property'nin serileştirilmemesini sağlar.
+        public Category Category { get; set; }
+    }
+}
