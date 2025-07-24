af7728afc672947aff5d8559179a4d9f5ce03d7b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-24 20:03:01 +0300

    Product & ProductImage DTO Process Added

--- Services/Catalog/MultiShop.Catalog/DTOs/ProductImageDTOs/ResultProductImageDTO.cs
+++ Services/Catalog/MultiShop.Catalog/DTOs/ProductImageDTOs/ResultProductImageDTO.cs
@@ -1,19 +1,14 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿using MultiShop.Catalog.Entities;
 
-namespace MultiShop.Catalog.Entities
+namespace MultiShop.Catalog.DTOs.ProductImageDTOs
 {
-    public class ProductImage
+    public class ResultProductImageDTO
     {
-        [BsonId] //Bu attribute, hangi property’nin MongoDB’de “_id” alanı olarak tutulacağını belirtir.
-
-        [BsonRepresentation(BsonType.ObjectId)] // Bu attribute, MongoDB’deki ObjectId türündeki verilerin string olarak temsil edilmesini sağlar.
         public string ProductImagesID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
         public string ProductImage1 { get; set; } //Birden fazla ürünün resmi varsa yolunu birden fazla tabloda tutacağız.
         public string ProductImage2 { get; set; } //İkinci resim için
         public string ProductImage3 { get; set; } //Üçüncü resim için
         public string ProductID { get; set; } //Ürün ile ilişkili hale getirmek için ProductID ekledik.
         public Product Product { get; set; } //Product tablosuyla ilişki ekledik.
-
     }
 }
--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,9 +1,17 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class ProductDetail
     {
+        [BsonId] //Bu attribute, hangi property’nin MongoDB’de “_id” alanı olarak tutulacağını belirtir.
+
+        [BsonRepresentation(BsonType.ObjectId)] // Bu attribute, MongoDB’deki ObjectId türündeki verilerin string olarak temsil edilmesini sağlar.
         public string ProductDetailID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
         public string ProductDetailDescription { get; set; } // Ürün Detay Açıklamasını Tuttuk.
-        public string ProductDetailInfo { get; set; } //Ürün Detay Bilgilerini Tuttuk.
+        public string ProductDetailInfo { get; set; } //Ürün Detay Bilgilerini Tuttuk.,
+        public string ProductID { get; set; } //Ürün ile ilişkili olduğu için Ürün ID'sini aldık.
+        public Product Product { get; set; } //Product tablosuyla ilişki ekledik.
     }
 }
