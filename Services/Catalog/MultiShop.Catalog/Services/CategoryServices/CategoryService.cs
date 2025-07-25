b9b6c8778ca83016379391bcd4a54bd02a401ecf
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-25 21:01:33 +0300

    15# Category Service Interface + Service Process Added

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.DTOs.CategoryDTOs;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection; // MongoDB'deki Category koleksiyonuna erişim sağlar.
+        private readonly IMapper _mapper; // AutoMapper kullanarak DTO'lar ve Entity'ler arasında dönüşüm yapar. AutoMapper kütüphanesi ile nesneler arası otomatik dönüşüm işlemlerinde kullanılır.
+
+        public Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDTO>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIDCategoryDTO> GetByIDCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.DTOs.CategoryDTOs;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDTO>> GetAllCategoryAsync(); // Tüm kategorileri getirir
+        Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO); // Yeni kategori oluşturur
+        Task UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO); // Kategoriyi günceller
+        Task DeleteCategoryAsync(string id); // Kategoriyi siler
+        Task<GetByIDCategoryDTO> GetByIDCategoryAsync(string id); // ID ile kategori getirir
+
+    }
+}
