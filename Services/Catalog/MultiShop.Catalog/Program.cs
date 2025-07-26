8b0660641a92837ace0579715f1610eaf536a367
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-26 16:34:40 +0300

    22# Catalog Configuretaion Process in Program.cs Added

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.Extensions.Options;
+using MultiShop.Catalog.Services.CategoryServices;
+using MultiShop.Catalog.Services.ProductImageServices;
+using MultiShop.Catalog.Services.ProductServices;
+using MultiShop.Catalog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>(); // Her istek için yeni bir CategoryService örneði oluþturur ve ICategoryService arayüzüne baðýmlýlýklarý bu sýnýfa yönlendirir (Dependency Injection).
+builder.Services.AddScoped<IProductService, ProductService>(); // Her istek için yeni bir ProductService örneði oluþturur ve IProductService arayüzüne baðýmlýlýklarý bu sýnýfa yönlendirir.
+builder.Services.AddScoped<IProductImageService, ProductImageService>(); // Her istek için yeni bir ProductImageService örneði oluþturur ve IProductImageService arayüzüne baðýmlýlýklarý bu sýnýfa yönlendirir.
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // Uygulamadaki tüm AutoMapper profillerini yükler ve otomatik eþleme (mapping) iþlemlerinin kullanýlmasýný saðlar.
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings")); // appsettings.json'daki DatabaseSettings bölümünü, DatabaseSettings sýnýfýna baðlar ve yapýlandýrýr.
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+}); // DatabaseSettings yapýlandýrmasýný IDatabaseSettings arayüzüyle Dependency Injection olarak saðlar.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
