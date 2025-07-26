f3a6f9ece7f6494c23cd992761ce9939292762cc
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-26 14:41:52 +0300

    17# Category Service CTOR Method Process Added

--- Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MultiShop.Catalog.DTOs.CategoryDTOs;
 using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
 
 namespace MultiShop.Catalog.Services.CategoryServices
 {
@@ -10,9 +11,19 @@ namespace MultiShop.Catalog.Services.CategoryServices
         private readonly IMongoCollection<Category> _categoryCollection; // MongoDB'deki Category koleksiyonuna erişim sağlar.
         private readonly IMapper _mapper; // AutoMapper kullanarak DTO'lar ve Entity'ler arasında dönüşüm yapar. AutoMapper kütüphanesi ile nesneler arası otomatik dönüşüm işlemlerinde kullanılır.
 
-        public Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO)
+        public CategoryService(IMapper mapper,IDatabaseSettings _databaseSettings)
         {
-            throw new NotImplementedException();
+            var client = new MongoClient(_databaseSettings.ConnectionString); // MongoDB ile bağlantı kurmak için istemci oluşturuluyor.
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); // Bağlantı kurulan MongoDB'de kullanılacak veritabanı seçiliyor.
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName); // Category koleksiyonu üzerinden işlem yapabilmek için referans alınıyor.
+            _mapper = mapper; // AutoMapper örneği sınıfın _mapper alanına atanıyor.
+                              //Dependency Injection ile gelen AutoMapper örneğini sınıfın içindeki değişkene atar.
+        }
+
+        public async Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO)
+        {
+            var values=_mapper.Map<Category>(createCategoryDTO); // CreateCategoryDTO nesnesini Category entity'sine dönüştürür.
+            await _categoryCollection.InsertOneAsync(values); // Dönüştürülen Category nesnesini MongoDB'deki koleksiyona ekler.
         }
 
         public Task DeleteCategoryAsync(string id)
