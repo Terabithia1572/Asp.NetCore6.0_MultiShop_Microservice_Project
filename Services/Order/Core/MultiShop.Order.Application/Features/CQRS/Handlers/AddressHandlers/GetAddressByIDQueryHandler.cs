8e6d9f41b3e7fca82713c2a511723bc6c75ee55c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-27 17:31:17 +0300

    39# Listing Addresses and Fetching by ID Handle Methods Added

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressByIDQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressByIDQueryHandler
+    {
+        private readonly IRepository<Address> _addressRepository;
+
+        public GetAddressByIDQueryHandler(IRepository<Address> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+        public async Task<GetAddressByIDQueryResult> Handle(GetAddressByIDQuery getAddressByIDQuery)
+        {
+            var values = await _addressRepository.GetByIDAsync(getAddressByIDQuery.AddressID); //AddressID sorgulanıyor
+            return new GetAddressByIDQueryResult // Sonuç döndürülüyor
+            {
+                AddressID = values.AddressID, // AddressID bilgisi
+                AddressCity = values.AddressCity, // AddressCity bilgisi
+                AddressDetail = values.AddressDetail, // AddressDetail bilgisi   
+                AddressDistrict = values.AddressDistrict, // AddressDistrict bilgisi
+                AddressUserID = values.AddressUserID // AddressUserID bilgisi
+            };
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressQueryHandler
+    {
+        private readonly IRepository<Address> _addressRepository; // Repository arayüzü kullanılarak Address nesneleri için veri erişimi sağlanır
+
+        public GetAddressQueryHandler(IRepository<Address> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+        public async Task<List<GetAddressQueryResult>> Handle()
+        {
+            var values = await _addressRepository.GetAllAsync(); // Tüm adresler asenkron olarak alınır
+            return values.Select(x => new GetAddressQueryResult // Her bir adres için sonuç nesnesi oluşturulur
+            {
+                AddressID = x.AddressID, // Adres ID'si
+                AddressCity = x.AddressCity, // Şehir bilgisi
+                AddressDetail = x.AddressDetail, // Detay bilgisi
+                AddressDistrict = x.AddressDistrict, // İlçe bilgisi
+                AddressUserID = x.AddressUserID // Kullanıcı ID'si
+            }).ToList(); // Sonuçlar listeye dönüştürülür
+        }
+    }
+}
