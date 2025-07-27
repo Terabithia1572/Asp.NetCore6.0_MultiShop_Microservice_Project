24152bc07025f2bd4d9e0d23a9e33f1b41f602b1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-27 17:37:45 +0300

    40# Address Remove & Update Handler Process Added

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class RemoveAddressCommandHandler
+    {
+        private readonly IRepository<Address> _addressRepository;
+
+        public RemoveAddressCommandHandler(IRepository<Address> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+        public async Task Handle(RemoveAddressCommand removeAddressCommand)
+        {
+            var values=await _addressRepository.GetByIDAsync(removeAddressCommand.AddressID); //AddressID'ye göre adresi al
+            await _addressRepository.DeleteAsync(values); // Alınan adresi sil
+        }
+    }
+
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _addressRepository;
+
+        public UpdateAddressCommandHandler(IRepository<Address> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+        public async Task Handle(UpdateAddressCommand updateAddressCommand)
+        {
+            var address = await _addressRepository.GetByIDAsync(updateAddressCommand.AddressID); // Adres ID'sine göre adresi al
+
+            if (address == null) // Eğer adres bulunamazsa
+            {
+                throw new Exception("Adres bulunamadı"); // Hata fırlat
+            }
+
+            address.AddressDetail = updateAddressCommand.AddressDetail; // Adres detayını güncelle
+            address.AddressCity = updateAddressCommand.AddressCity; // Şehir bilgisini güncelle
+            address.AddressDistrict = updateAddressCommand.AddressDistrict; // İlçe bilgisini güncelle
+            address.AddressUserID = updateAddressCommand.AddressUserID; // Kullanıcı ID'sini güncelle
+            await _addressRepository.UpdateAsync(address); // Güncellenmiş adresi veritabanında güncelle
+        }
+    }
+}
