c9cf28c38cc2b4d36344cecb2793f66943fd395c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-28 21:36:05 +0300

    52# Calling the Service Registiration in program.cs Process Added

--- Services/Order/Presentation/MultiShop.Order.WebAPI/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebAPI/Program.cs
@@ -1,7 +1,14 @@
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped(typeof(IRepository<>),typeof(IRepository<>)); // IRepository arayüzünü uygulayan tüm sýnýflar için DI konteynerine ekler.
+builder.Services.AddApplicationServices(builder.Configuration); // Uygulama katmanýndaki servislerin DI konteynerine eklenmesi için kullanýlýr. Bu, uygulama katmanýndaki tüm servislerin Dependency Injection ile kullanýlabilmesini saðlar.
+
+#region
 
 builder.Services.AddScoped<GetAddressByIDQueryHandler>();
 // Her HTTP isteðinde yeni bir GetAddressByIDQueryHandler nesnesi oluþturur ve Dependency Injection ile saðlar.
@@ -33,6 +40,7 @@ builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
 builder.Services.AddScoped<GetOrderDetailByIDQueryHandler>();
 // Her HTTP isteðinde yeni bir GetOrderDetailByIDQueryHandler nesnesi oluþturur ve Dependency Injection ile saðlar.
 
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
