55e92578ef4d8490a0253e1aea1af6ad3d406bd8
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-28 21:24:34 +0300

    51# Service Registeration Process Added

---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Services
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddApplicationServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            // Uygulama katmanındaki servislerin DI konteynerine eklenmesi için kullanılır.
+            // Burada, uygulama katmanındaki tüm servisler eklenebilir.
+            // Örneğin:
+            // services.AddScoped<IOrderingService, OrderingService>();
+            // services.AddScoped<IProductService, ProductService>();
+            // services.AddScoped<ICustomerService, CustomerService>();
+
+            services.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly)); // MediatR kütüphanesini kullanarak, uygulama katmanındaki tüm komut ve sorgu handler'larını kaydeder.
+            // MediatR, CQRS (Command Query Responsibility Segregation) mimarisini uygulamak için kullanılır.
+            // Bu sayede, uygulama katmanındaki komut ve sorguların handler'ları otomatik olarak DI konteynerine eklenir.
+
+        }
+    }
+}
