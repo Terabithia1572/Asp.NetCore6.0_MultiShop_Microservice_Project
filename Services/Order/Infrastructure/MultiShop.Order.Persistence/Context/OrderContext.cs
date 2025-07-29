64146ca42491f787c83037af6152f303fc48f973
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-29 21:54:16 +0300

    62# OrderDB Migration Process With Docker Process Added

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -12,7 +12,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=.;initial Catalog=MultiShopOrderDB;integrated Security=true");
+            optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopOrderDB;User=sa;Password=Yunus6565*");
 
         }
         public DbSet<Address> Addresses { get; set; }
