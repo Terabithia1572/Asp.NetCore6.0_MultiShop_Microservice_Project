9337844c101530589f6a55d75f9c19c7b6f21992
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-30 21:46:03 +0300

    72# User Register DTO & Registers Controller Process Added

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.DTOs;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager; // UserManager, Identity framework'ün kullanıcı yönetimi için kullanılan bir sınıfıdır.
+
+        public RegistersController(UserManager<ApplicationUser> userManager) // UserManager, ApplicationUser türünde kullanıcıları yönetmek için kullanılır.
+        {
+            _userManager = userManager; // UserManager'ı enjekte ediyoruz.
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDTO userRegisterDTO)
+        {
+            var values = new ApplicationUser // Yeni bir ApplicationUser nesnesi oluşturuyoruz.
+            {
+                UserName = userRegisterDTO.Username, // Kullanıcı adını DTO'dan alıyoruz.
+                Email = userRegisterDTO.Email, // E-posta adresini DTO'dan alıyoruz.
+                Name = userRegisterDTO.Name, // Adı DTO'dan alıyoruz.
+                Surname = userRegisterDTO.Surname // Soyadı DTO'dan alıyoruz.
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDTO.Password); // Kullanıcıyı oluşturuyoruz ve sonucu alıyoruz.
+            if (result.Succeeded) // Eğer kullanıcı oluşturma başarılı ise
+            {
+                return Ok("Kullanıcı Başarıyla Eklendi.."); // 200 OK döndürüyoruz.
+            }
+            else // Eğer kullanıcı oluşturma başarısız ise
+            {
+                return BadRequest("Bir Hata Meydana Geldi: "+result.Errors); // Hataları içeren bir BadRequest döndürüyoruz.
+            }
+        }
+    
+}
+}
---
+++ IdentityServer/MultiShop.IdentityServer/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.IdentityServer.DTOs
+{
+    public class UserRegisterDTO //Kullanıcı Kayıt DTO'su
+    {
+        public string Username { get; set; } // Kullanıcı adı
+        public string Password { get; set; } // Şifre
+        public string Email { get; set; } // E-posta adresi
+        public string Name { get; set; } // Ad
+        public string Surname { get; set; } // Soyad
+    }
+}
