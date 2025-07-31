1f42256b823a27a8b624e6e8f7d787607f5052f0
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-31 21:03:01 +0300

    79# Discount & Order Permission Process Added

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -14,7 +14,21 @@ namespace MultiShop.IdentityServer
     new ApiResource("ResourceCatalog")
     {
         Scopes = { "CatalogFullPermission", "CatalogReadPermission" }
+    },
+    new ApiResource("ResourceDiscount")
+    {
+        Scopes = { "DiscountFullPermission", "DiscountReadPermission" }
+    },
+    new ApiResource("ResourceOrder")
+    {
+        Scopes = { "OrderFullPermission", "OrderReadPermission" }
     }
+        // API kaynaklarını tanımlar. Her bir API kaynağı, erişim izinlerini (scopes) içerir.
+        // "ResourceCatalog": Katalog API'si için bir kaynak tanımlanır.
+        // "ResourceDiscount": İndirim API'si için bir kaynak tanımlanır.
+        // "ResourceOrder": Sipariş API'si için bir kaynak tanımlanır.
+        // Her kaynak, ilgili erişim izinlerini (scopes) içerir.
+        
     };
         // "ResourceCatalog" adında bir API kaynağı tanımlanır. 
         // Bu API kaynağına erişmek için iki farklı yetki kapsamı (scope) belirlenmiştir:
@@ -35,7 +49,11 @@ namespace MultiShop.IdentityServer
         public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
         {
     new ApiScope("CatalogFullPermission", "Full access to the Catalog API"),
-    new ApiScope("CatalogReadPermission", "Read access to the Catalog API")
+    new ApiScope("CatalogReadPermission", "Read access to the Catalog API"),
+    new ApiScope("DiscountFullPermission", "Full access to the Discount API"),
+    new ApiScope("DiscountReadPermission", "Read access to the Discount API"),
+    new ApiScope("OrderFullPermission", "Full access to the Order API"),
+    new ApiScope("OrderReadPermission", "Read access to the Order API")
         };
         // API'ye erişim kapsamlarını (scopes) tanımlar.
         // "CatalogFullPermission": Katalog API'sine tam yetkili erişimi temsil eder.
