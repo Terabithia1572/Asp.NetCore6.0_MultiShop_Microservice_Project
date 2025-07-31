7939a478a4453fce8292e072b18a2f909a2da797
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-31 22:59:41 +0300

    82# Get Token Process Added

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using IdentityServer4;
+﻿using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -10,108 +6,98 @@ namespace MultiShop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
-    {
-    new ApiResource("ResourceCatalog")
-    {
-        Scopes = { "CatalogFullPermission", "CatalogReadPermission" }
-    },
-    new ApiResource("ResourceDiscount")
-    {
-        Scopes = { "DiscountFullPermission", "DiscountReadPermission" }
-    },
-    new ApiResource("ResourceOrder")
-    {
-        Scopes = { "OrderFullPermission", "OrderReadPermission" }
-    }
         // API kaynaklarını tanımlar. Her bir API kaynağı, erişim izinlerini (scopes) içerir.
         // "ResourceCatalog": Katalog API'si için bir kaynak tanımlanır.
         // "ResourceDiscount": İndirim API'si için bir kaynak tanımlanır.
         // "ResourceOrder": Sipariş API'si için bir kaynak tanımlanır.
         // Her kaynak, ilgili erişim izinlerini (scopes) içerir.
-        
-    };
-        // "ResourceCatalog" adında bir API kaynağı tanımlanır. 
-        // Bu API kaynağına erişmek için iki farklı yetki kapsamı (scope) belirlenmiştir:
-        // 1. "CatalogFullPermission": Katalog API'sine tam erişim izni sağlar (okuma, yazma, güncelleme, silme).
-        // 2. "CatalogReadPermission": Katalog API'sini sadece okuma (read) izniyle kullanmaya imkan tanır.
-
-        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
-    new IdentityResources.OpenId(),
-    new IdentityResources.Email(),
-    new IdentityResources.Profile()
+            new ApiResource("ResourceCatalog")
+            {
+                Scopes = { "CatalogFullPermission", "CatalogReadPermission" }
+            },
+            new ApiResource("ResourceDiscount")
+            {
+                Scopes = { "DiscountFullPermission" }
+            },
+            new ApiResource("ResourceOrder")
+            {
+                Scopes = { "OrderFullPermission" }
+            },
         };
+
         // Kimlik doğrulama sırasında kullanıcının kimliğini ve temel bilgilerini içeren standart identity kaynakları tanımlanır.
         // OpenId: Kullanıcı kimliğini (id) sağlar. Kimlik doğrulamanın temelidir ve zorunludur.
         // Email: Kullanıcının e-posta bilgisini sağlar.
         // Profile: Kullanıcının profil bilgilerini (ad, soyad, vs.) sağlar.
-
-        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
         {
-    new ApiScope("CatalogFullPermission", "Full access to the Catalog API"),
-    new ApiScope("CatalogReadPermission", "Read access to the Catalog API"),
-    new ApiScope("DiscountFullPermission", "Full access to the Discount API"),
-    new ApiScope("DiscountReadPermission", "Read access to the Discount API"),
-    new ApiScope("OrderFullPermission", "Full access to the Order API"),
-    new ApiScope("OrderReadPermission", "Read access to the Order API")
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
         };
+
         // API'ye erişim kapsamlarını (scopes) tanımlar.
         // "CatalogFullPermission": Katalog API'sine tam yetkili erişimi temsil eder.
         // "CatalogReadPermission": Sadece okuma yetkisiyle erişimi temsil eder.
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogFullPermission", "Full access to the Catalog API"),
+            new ApiScope("CatalogReadPermission", "Read access to the Catalog API"),
+            new ApiScope("DiscountFullPermission", "Full access to the Discount API"),
+            new ApiScope("DiscountReadPermission", "Read access to the Discount API"),
+            new ApiScope("OrderFullPermission", "Full access to the Order API"),
+            new ApiScope("OrderReadPermission", "Read access to the Order API")
+        };
 
+        // IdentityServer için kullanılacak istemci (client) uygulamaları tanımlanır.
+        // Her client'ın hangi grant type'ları ve hangi scope'ları (izinleri) kullanabileceği belirtilir.
         public static IEnumerable<Client> Clients => new Client[]
- {
-    new Client
-    {
-        ClientId = "MultiShopVisitorID", // İstemcinin (client) benzersiz kimliği.
-        ClientName = "MultiShop Visitor User", // İstemcinin açıklayıcı adı.
-        AllowedGrantTypes = GrantTypes.ClientCredentials, // Kimlik doğrulama için kullanılan grant type (client credentials akışı).
-        ClientSecrets =
         {
-            new Secret("multishopsecret".Sha256()) // Client için güvenlik anahtarı (SHA-256 ile şifrelenmiş).
-        },
-        AllowedScopes =
-        {
-            "CatalogReadPermission" // Bu client yalnızca "CatalogReadPermission" yetkisiyle API'ye erişebilir.
-        }
-    },
-    new Client {
-    ClientId = "MultiShopManagerID", // İstemcinin (client) benzersiz kimliği.
-    ClientName = "MultiShop Manager User", // İstemcinin açıklayıcı adı.
-    AllowedGrantTypes = GrantTypes.Code, // Kimlik doğrulama için kullanılan grant type (authorization code akışı).
-        AllowedScopes =
-        {
-            "CatlogReadPermission", // Bu client, Katalog API'sine okuma izni ile erişebilir.
-            "CatalogFullPermission", // Katalog API'sine tam erişim izni ile erişebilir.
-        }
-    
-    },
-    new Client {
-    ClientId="MultiShopAdminID", // İstemcinin (client) benzersiz kimliği.
-    ClientName = "MultiShop Admin User", // İstemcinin açıklayıcı adı.
-    AllowedGrantTypes = GrantTypes.Code, // Kimlik doğrulama için kullanılan grant type (authorization code akışı).
-    ClientSecrets={new Secret("multishopsecret".Sha256())},
-    AllowedScopes={
-            "CatalogFullPermission", // Bu client, Katalog API'sine tam erişim izni ile erişebilir.
-            "DiscountFullPermission", // İndirim API'sine tam erişim izni ile erişebilir.
-            "OrderFullPermission", // Sipariş API'sine tam erişim izni ile erişebilir.
-            "CatalogReadPermission", // Katalog API'sine okuma izni ile erişebilir.
-            IdentityServerConstants.LocalApi.ScopeName, // Local API erişimi için gerekli izin.
-            IdentityServerConstants.StandardScopes.OpenId, // OpenID kimlik doğrulama izni.
-            IdentityServerConstants.StandardScopes.Profile, // Kullanıcı profili bilgilerine erişim izni.
-            IdentityServerConstants.StandardScopes.Email // Kullanıcı e-posta bilgilerine erişim izni.
+            // Sadece katalog okuma yetkisine sahip, client credentials ile erişen client
+            new Client
+            {
+                ClientId = "MultiShopVisitorID", // İstemcinin (client) benzersiz kimliği.
+                ClientName = "MultiShop Visitor User", // İstemcinin açıklayıcı adı.
+                AllowedGrantTypes = GrantTypes.ClientCredentials, // Kimlik doğrulama için kullanılan grant type (client credentials akışı).
+                ClientSecrets = { new Secret("multishopsecret".Sha256()) }, // Client için güvenlik anahtarı (SHA-256 ile şifrelenmiş).
+                AllowedScopes = { "CatalogReadPermission" } // Bu client yalnızca "CatalogReadPermission" yetkisiyle API'ye erişebilir.
+            },
 
-        },
-    AccessTokenLifetime = 600, // Erişim token'ının ömrü (saniye cinsinden).
+            // Katalog yönetici paneli için, kod akışıyla giriş yapan client (kullanıcı kimliğiyle giriş)
+            new Client
+            {
+                ClientId = "MultiShopManagerID", // İstemcinin (client) benzersiz kimliği.
+                ClientName = "MultiShop Manager User", // İstemcinin açıklayıcı adı.
+                AllowedGrantTypes = GrantTypes.Code, // Kimlik doğrulama için kullanılan grant type (authorization code akışı).
+                AllowedScopes =
+                {
+                    "CatalogReadPermission", // Katalog API'sine okuma izni ile erişebilir.
+                    "CatalogFullPermission"  // Katalog API'sine tam erişim izni ile erişebilir.
+                }
+            },
 
-
-    }
-
-    //
-
-
- };
-
+            // Yönetici uygulaması için, client credentials ile tam yetkili erişimi olan client
+            new Client
+            {
+                ClientId = "MultiShopAdminID", // İstemcinin (client) benzersiz kimliği.
+                ClientName = "MultiShop Admin User", // İstemcinin açıklayıcı adı.
+                AllowedGrantTypes = GrantTypes.ClientCredentials, // (DÜZELTİLDİ) Kimlik doğrulama için client credentials grant type kullanılır.
+                ClientSecrets = { new Secret("multishopsecret".Sha256()) }, // Client için güvenlik anahtarı (SHA-256 ile şifrelenmiş).
+                AllowedScopes =
+                {
+                    "CatalogFullPermission",       // Katalog API'sine tam erişim
+                    "DiscountFullPermission",      // İndirim API'sine tam erişim
+                    "OrderFullPermission",         // Sipariş API'sine tam erişim
+                    "CatalogReadPermission",       // Katalog API'sine okuma erişimi
+                    IdentityServerConstants.LocalApi.ScopeName,         // Local API erişimi
+                    IdentityServerConstants.StandardScopes.OpenId,      // OpenID kimlik doğrulama
+                    IdentityServerConstants.StandardScopes.Profile,     // Kullanıcı profili bilgileri
+                    IdentityServerConstants.StandardScopes.Email        // Kullanıcı e-posta bilgileri
+                },
+                AccessTokenLifetime = 600 // Erişim token'ının ömrü (saniye cinsinden).
+            }
+        };
     }
 }
