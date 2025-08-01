37044b0005f263ad0cff0d94f4360ae734bd80f6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-01 22:00:41 +0300

    88# Protected with Discount Microservice Token Process Added

--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.Discount.Context;
 using MultiShop.Discount.Services.DiscountService;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) // JWT Bearer kimlik doðrulamasýný kullanýr.
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServerUrl"]; // IdentityServer URL'sini yapýlandýrma dosyasýndan alýr.
+        options.Audience = "ResourceDiscount"; // Bu API'nin Audience'ýný tanýmlar.
+        options.RequireHttpsMetadata = false; // HTTPS gereksinimini devre dýþý býrakýr (geliþtirme ortamýnda kullanýlabilir).
+    });
+
 
 // Add services to the container.
 builder.Services.AddTransient<DapperContext>();
@@ -26,7 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication(); // Kimlik doðrulama middleware'ini kullanýr, bu middleware JWT Bearer token'larýný doðrular.
 app.UseAuthorization();
 
 app.MapControllers();
