a5c8537e7147a56bdbd2cbf6ff22c08ba3250650
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-01 21:44:21 +0300

    86# Bearer Token App With Catalog Microservice Process Added

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using MultiShop.Catalog.Services.CategoryServices;
 using MultiShop.Catalog.Services.ProductDetailServices;
@@ -7,6 +8,14 @@ using MultiShop.Catalog.Settings;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) // JWT Bearer kimlik doðrulamasýný kullanýr.
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServerUrl"]; // IdentityServer URL'sini yapýlandýrma dosyasýndan alýr.
+        options.Audience = "ResourceCatalog"; // Bu API'nin Audience'ýný tanýmlar.
+        options.RequireHttpsMetadata = false; // HTTPS gereksinimini devre dýþý býrakýr (geliþtirme ortamýnda kullanýlabilir).
+    });
 
 builder.Services.AddScoped<ICategoryService, CategoryService>(); // Her istek için yeni bir CategoryService örneði oluþturur ve ICategoryService arayüzüne baðýmlýlýklarý bu sýnýfa yönlendirir (Dependency Injection).
 builder.Services.AddScoped<IProductService, ProductService>(); // Her istek için yeni bir ProductService örneði oluþturur ve IProductService arayüzüne baðýmlýlýklarý bu sýnýfa yönlendirir.
@@ -38,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Kimlik doðrulama middleware'ini kullanýr, bu middleware JWT Bearer token'larýný doðrular.
 
 app.UseAuthorization();
 
