b71ab51d53baee149cb325351afe209eac5be0e1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-02 20:58:26 +0300

    94# Cargo DB Database Migration Process Added

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Migrations/20250802175530_mig1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Cargo.DataAccessLayer.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CargoCompanies",
+                columns: table => new
+                {
+                    CargoCompanyID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CargoCompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoCompanies", x => x.CargoCompanyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CargoCustomers",
+                columns: table => new
+                {
+                    CargoCustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CargoCustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoCustomerSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoCustomerEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoCustomerPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoCustomerCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoCustomerDistrict = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoCustomerAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoCustomers", x => x.CargoCustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CargoOperations",
+                columns: table => new
+                {
+                    CargoOperationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoOperationDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoOperationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoOperations", x => x.CargoOperationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CargoDetails",
+                columns: table => new
+                {
+                    CargoDetailID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Barcode = table.Column<int>(type: "int", nullable: false),
+                    CargoCompanyID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoDetails", x => x.CargoDetailID);
+                    table.ForeignKey(
+                        name: "FK_CargoDetails_CargoCompanies_CargoCompanyID",
+                        column: x => x.CargoCompanyID,
+                        principalTable: "CargoCompanies",
+                        principalColumn: "CargoCompanyID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CargoDetails_CargoCompanyID",
+                table: "CargoDetails",
+                column: "CargoCompanyID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CargoCustomers");
+
+            migrationBuilder.DropTable(
+                name: "CargoDetails");
+
+            migrationBuilder.DropTable(
+                name: "CargoOperations");
+
+            migrationBuilder.DropTable(
+                name: "CargoCompanies");
+        }
+    }
+}
