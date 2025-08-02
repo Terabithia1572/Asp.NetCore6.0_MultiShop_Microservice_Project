ea30bea24685e8e6813381d99a956c79742424b3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-02 20:53:01 +0300

    93# Cargo Context Process Added

---
+++ Services/Cargo/MultiShop.Cargo.EntityLayer/Concrete/CargoOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.EntityLayer.Concrete
+{
+    public class CargoOperation
+    {
+        public int CargoOperationID { get; set; } //Kargo Hareketleri ID
+        public string Barcode { get; set; } //Kargo Barkod numarası
+        public string CargoOperationDescription { get; set; } //Kargo Hareket Açıklaması
+        public DateTime CargoOperationDate { get; set; } //Kargo Hareket Tarihi
+    }
+}
