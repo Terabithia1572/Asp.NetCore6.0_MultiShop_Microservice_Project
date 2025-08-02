8b31648b637498bcbedab22e50405f52591f387b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-02 20:33:31 +0300

    92# Cargo Entities Process Added

---
+++ Services/Cargo/MultiShop.Cargo.EntityLayer/Concrete/CargoCompany.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.EntityLayer.Concrete
+{
+    public class CargoCompany
+    {
+        public int CargoCompanyID { get; set; } //Kargo Şirketi ID
+        public string CargoCompanyName { get; set; } // Kargo Şirketi Adı
+        
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.EntityLayer/Concrete/CargoDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.EntityLayer.Concrete
+{
+    public class CargoDetail
+    {
+        public int CargoDetailID { get; set; } //Kargo Detay ID
+        public string SenderCustomer { get; set; } //Gönderen Müşteri 
+        public string ReceiverCustomer { get; set; } // Alıcı Müşteri 
+        public int Barcode { get; set; } //Barkod Numarası
+        public int CargoCompanyID { get; set; } //Kargo Şirket ID
+        public CargoCompany CargoCompany { get; set; } // Kargo Şirket Bilgileri
+
+    }
+}
