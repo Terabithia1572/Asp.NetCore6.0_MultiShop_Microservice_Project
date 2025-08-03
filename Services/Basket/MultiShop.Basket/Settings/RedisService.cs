b19014faaab842cc167ed63b396afe18ee4abbdf
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-03 21:02:31 +0300

    111# Redis Service Connection Class Process Added

---
+++ Services/Basket/MultiShop.Basket/Settings/RedisService.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace MultiShop.Basket.Settings
+{
+    public class RedisService
+    {
+        public string _host { get; set; } // Redis sunucusunun adresi
+        public int _port { get; set; } // Redis sunucusunun portu
+        private ConnectionMultiplexer _connectionMultiplexer;
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() =>_connectionMultiplexer= ConnectionMultiplexer.Connect($"{_host}:{_port}"); // Redis sunucusuna bağlantı kurar
+        public IDatabase GetDB(int db=1)=>_connectionMultiplexer.GetDatabase(0); // Belirtilen veritabanını alır, varsayılan olarak 1. veritabanı kullanılır
+
+    }
+}
