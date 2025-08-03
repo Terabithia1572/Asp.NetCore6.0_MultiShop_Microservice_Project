9c4711775641eb9e268073af897e651396d42549
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-03 21:18:59 +0300

    112# Basket Service Class Process Added

---
+++ Services/Basket/MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Basket.DTOs.BasketTotalDTOs;
+using MultiShop.Basket.Settings;
+using System.Text.Json;
+
+namespace MultiShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string UserID)
+        {
+            await _redisService.GetDB().KeyDeleteAsync(UserID); // Belirli bir kullanıcının sepetini siler
+        }
+
+        public async Task<BasketTotalDTO> GetBasket(string UserID)
+        {
+           var existBasket=await _redisService.GetDB().StringGetAsync(UserID); // belirli bir kullanıcının sepetini getirir
+            return JsonSerializer.Deserialize<BasketTotalDTO>(existBasket); // Sepeti JSON'dan nesneye dönüştürür
+
+        }
+
+        public async Task SaveBasket(BasketTotalDTO basketTotalDTO)
+        {
+            await _redisService.GetDB().StringSetAsync(basketTotalDTO.UserID, JsonSerializer.Serialize(basketTotalDTO)); // Sepeti kaydeder veya günceller
+        }
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Basket.DTOs.BasketTotalDTOs;
+
+namespace MultiShop.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDTO> GetBasket(string UserID); // belirli bir kullanıcının sepetini getirir
+        Task SaveBasket(BasketTotalDTO basketTotalDTO); // Sepeti kaydeder veya günceller
+        Task DeleteBasket(string UserID); // Belirli bir kullanıcının sepetini siler
+    }
+}
