d72779c6dde55e0b4fcb2da5d03497b8168b243a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-03 20:46:56 +0300

    109# Basket DTO Process Added

---
+++ Services/Basket/MultiShop.Basket/DTOs/BasketItemDTOs/BasketItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Basket.DTOs.BasketItemDTOs
+{
+    public class BasketItemDTO
+    {
+        public string ProductID { get; set; } //Ürün ID'si
+        public string ProductName { get; set; } //Ürün Adı
+        public int ProductQuantity { get; set; } //Ürün Miktarı
+        public decimal ProductPrice { get; set; } //Ürün Fiyatı
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/DTOs/BasketTotalDTOs/BasketTotalDTO.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Basket.DTOs.BasketItemDTOs;
+
+namespace MultiShop.Basket.DTOs.BasketTotalDTOs
+{
+    public class BasketTotalDTO
+    {
+        public string UserID { get; set; } //Kullanıcı ID
+        public string DiscountCode { get; set; } //İndirim Kodu
+        public int? DiscountRate { get; set; }  // İndirim Oranı
+        public List<BasketItemDTO> BasketItems { get; set; } // Sepetteki Ürünler
+        public decimal TotalPrice { get => BasketItems.Sum(x => x.ProductPrice * x.ProductQuantity); }
+
+    }
+}
