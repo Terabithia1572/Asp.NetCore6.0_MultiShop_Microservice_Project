ca2df06deb41592f0300f3a09848153367ec8d14
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-03 16:26:26 +0300

    103# Cargo Program.cs Depency Injection & Registiration Process Added

--- Services/Cargo/MultiShop.Cargo.WebAPI/Program.cs
+++ Services/Cargo/MultiShop.Cargo.WebAPI/Program.cs
@@ -1,6 +1,20 @@
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.BusinessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<CargoContext>(); //Dbcontext'e Depency Injection ile ekleniyor
+builder.Services.AddScoped<ICargoCompanyDal,EfCargoCompanyDal>(); // CargoCompany için EntityFramework implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoCompanyService, CargoCompanyManager>(); // CargoCompany için BusinessLayer implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoCustomerDal, EfCargoCustomerDal>(); // CargoCustomer için EntityFramework implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoCustomerService, CargoCustomerManager>(); // CargoCustomer için BusinessLayer implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoDetailDal, EfCargoDetailDal>(); // CargoDetail için EntityFramework implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoDetailService, CargoDetailManager>(); // CargoDetail için BusinessLayer implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoOperationDal, EfCargoOperationDal>(); // CargoOperation için EntityFramework implementasyonu ekleniyor
+builder.Services.AddScoped<ICargoOperationService, CargoOperationManager>(); // CargoOperation için BusinessLayer implementasyonu ekleniyor
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
