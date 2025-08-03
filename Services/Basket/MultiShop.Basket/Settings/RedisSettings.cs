34a961b84f9f8069b3941cdb597d79f2f4584bbd
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-03 20:52:20 +0300

    110# Redis Container & AppSettingJson Settings Process Added

---
+++ Services/Basket/MultiShop.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Basket.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; } // Redis sunucusunun adresi
+        public int Port { get; set; } // Redis sunucusunun portu
+    }
+}
