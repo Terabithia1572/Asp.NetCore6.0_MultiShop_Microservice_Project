3e09247a510544553ce74ffa6452b6ebc4a08bac
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-03 14:57:26 +0300

    95# Cargo Generic Dal & Interfaces Process Added

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity); // Yeni bir varlığı (entity) veritabanına ekler.
+
+        void Update(T entity); // Var olan bir varlığı (entity) veritabanında günceller.
+
+        void Delete(int id); // Verilen ID'ye sahip varlığı (entity) veritabanından siler.
+
+        T GetByID(int id); // Verilen ID'ye sahip varlığı (entity) veritabanından getirir.
+
+        List<T> GetAll(); // Tüm varlıkları (entity) veritabanından getirir.
+    }
+
+}
