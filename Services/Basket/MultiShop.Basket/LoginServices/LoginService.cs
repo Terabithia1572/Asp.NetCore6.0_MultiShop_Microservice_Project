4698ae0f7cdcebfb12d7f1b9ecb39c1ab22a2c67
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-04 20:41:22 +0300

    113# Getting the sub login value of the logged in user

---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor; // IHttpContextAccessor, HTTP isteklerine erişim sağlar
+
+        public LoginService(IHttpContextAccessor httpContextAccessor) // IHttpContextAccessor, HTTP isteklerine erişim sağlar
+        {
+            _httpContextAccessor = httpContextAccessor; // IHttpContextAccessor, HTTP isteklerine erişim sağlar
+        }
+
+        public string GetUserID => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value; // Kullanıcının ID'sini JWT'den alır
+    }
+}
