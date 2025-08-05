3ff491ca48b3ee7dedb4eac9af48556b147fa115
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-05 21:22:20 +0300

    122# Header Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/LayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.LayoutViewComponents
+{
+    public class _HeadUILayoutComponentPartial: ViewComponent // Burada ViewComponent sınıfından türetiliyor
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu ViewComponent'in nasıl çağrılacağını belirler
+        {
+            return View(); // View() metodu, ilgili View dosyasını döndürür
+        }
+    }
+}
