8509b2a83e32520cca66e90a5a9dbc519b09bde6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-07 22:12:55 +0300

    133# Footer Layout Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() //bu metot, bu view component çağrıldığında çalışır.
+        {
+            return View(); // View() metodu, bu view component için varsayılan view dosyasını döndürür.
+        }
+    }
+}
