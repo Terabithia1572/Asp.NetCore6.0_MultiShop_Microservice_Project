32a4b727a095eb7270b3f025e72f84e4930d3c31
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-07 21:13:54 +0300

    124# Navbar UI Layout View Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() //bu metod, bu view component çağrıldığında çalışır
+        {
+            return View(); // View() metodu, bu view component için varsayılan view'ı döndürür.
+        }
+    }
+}
