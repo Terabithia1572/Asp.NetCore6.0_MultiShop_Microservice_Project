b3baf20ed2ecd17f25b33e46bbee2b28d5093384
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-07 21:42:18 +0300

    127# Special Offer & Discount Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _SpecialOfferComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() //bu metod, bu view component çağrıldığında çalışır
+        {
+            return View(); // View() metodu, bu view component için varsayılan view'ı döndürür.
+        }
+    }
+}
