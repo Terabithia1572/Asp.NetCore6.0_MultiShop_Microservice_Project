753bf301940fc55fe22c7d65b5fb725d9f534cc1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-08 20:58:56 +0300

    134# Script Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptUILayoutComponentPartial: ViewComponent // ViewComponent sınıfından türetilir
+    {
+        public IViewComponentResult Invoke() // bu metod, ViewComponent çağrıldığında çalışır
+        {
+            return View(); // View() metodu, varsayılan olarak "_ScriptUILayoutComponentPartial.cshtml" dosyasını arar ve render eder
+        }
+    }
+}
