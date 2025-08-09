1e9b951e8c813cd320c185e4f09b23b4402fafc2
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 16:31:10 +0300

    144# Shopping Cart Product List Component Process Added + New Shopping Cart css Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartDiscountCouponComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponents
+{
+    public class _ShoppingCartDiscountCouponComponentPartial:ViewComponent //bu sınıf ViewComponent sınıfından türetilmiştir
+    {
+        public IViewComponentResult Invoke() //Invoke metodu, ViewComponent'ın çağrıldığında ne yapacağını belirler
+        {
+            return View(); //View() metodu, varsayılan olarak "_ShoppingCartDiscountCouponComponentPartial.cshtml" dosyasını render eder
+        }
+    }
+   
+}
