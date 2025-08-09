93c3cf298daaa4e1c9cb19e580c52c71d198d52f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 15:59:47 +0300

    141# Product Detail Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailDescriptionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailDescriptionComponentPartial: ViewComponent // bu sınıf ViewComponent sınıfından türetilmiştir
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu ViewComponent çağrıldığında çalışacak metottur
+        {
+            return View(); // View() metodu, varsayılan olarak _ProductDetailDescriptionComponentPartial.cshtml dosyasını render eder
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailIformationComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailIformationComponentPartial: ViewComponent //bu sınıf ViewComponent sınıfından türetilir 
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu ViewComponent çağrıldığında çalışacak metottur.
+        {
+            return View(); // View() metodu, varsayılan olarak _ProductDetailIformationComponentPartial.cshtml dosyasını render eder.
+        }
+    }
+}
