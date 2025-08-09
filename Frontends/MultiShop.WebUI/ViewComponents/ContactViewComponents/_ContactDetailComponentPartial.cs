4f190dced287ef5a53fe4177a1bf61c9c6f6b90a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 16:44:15 +0300

    145# Contact Page Design Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ContactViewComponents/_ContactDetailComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ContactViewComponents
+{
+    public class _ContactDetailComponentPartial: ViewComponent //bu sınıf ViewComponent sınıfından türetilmiştir
+    {
+        public IViewComponentResult Invoke() //Invoke metodu, ViewComponent'ın çağrıldığında ne yapacağını belirler
+        {
+            return View(); //View() metodu, varsayılan olarak "_ContactDetailComponentPartial.cshtml" dosyasını render eder
+        }
+    }
+}
