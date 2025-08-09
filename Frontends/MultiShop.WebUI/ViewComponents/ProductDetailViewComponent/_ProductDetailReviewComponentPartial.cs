49a56810f5f0d352adcfb5e2840d52103a8af733
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 16:04:15 +0300

    142# Product Review Component Partial Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailReviewComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailReviewComponentPartial:ViewComponent //bu sınıf ViewComponent sınıfından türetilir 
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu ViewComponent çağrıldığında çalışacak metottur.
+        {
+            return View(); // View() metodu, varsayılan olarak _ProductDetailReviewComponentPartial.cshtml dosyasını render eder.
+        }
+    }
+   
+}
