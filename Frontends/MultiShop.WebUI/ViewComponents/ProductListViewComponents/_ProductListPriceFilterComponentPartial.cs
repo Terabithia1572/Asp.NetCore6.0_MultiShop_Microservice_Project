e351a90221c927224898d77574237db06f39e80f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 14:21:48 +0300

    135# Filter By Price Design Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListPriceFilterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListPriceFilterComponentPartial: ViewComponent // bu metot parçası, fiyat filtresi için kullanılır
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu view component çağrıldığında çalışır
+        {
+           
+            return View(); // View() metodu, ilgili view dosyasını döndürür
+        }
+    }
+}
