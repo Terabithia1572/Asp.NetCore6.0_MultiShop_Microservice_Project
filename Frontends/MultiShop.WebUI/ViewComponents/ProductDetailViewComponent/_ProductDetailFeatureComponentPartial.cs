19ba46ef1d2ec5f80ecf3b2d0ca99304bb80fb86
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 15:51:17 +0300

    140# Product Feature Process Add in Product Detail Page

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailFeatureComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailFeatureComponentPartial: ViewComponent // Bu sınıf, ürün detayları için bir görüntü bileşeni sağlar
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Burada, ürün detayları için gerekli verileri alabilir ve görüntüleyebilirsiniz.
+            // Örneğin, bir model veya veri kaynağı kullanabilirsiniz.
+            // Şu anda sadece örnek bir görünüm döndürüyoruz.
+            return View();
+        }
+    }
+}
