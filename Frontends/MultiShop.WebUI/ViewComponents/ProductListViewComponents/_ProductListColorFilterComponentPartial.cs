9d9f9508a4df4697ea34efe1e1a840badd91af98
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-09 14:42:16 +0300

    136# Filter By Size & Color  View Component Process Added

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListColorFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListColorFilterComponentPartial: ViewComponent // bu metot parçası, renk filtresi için kullanılır
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu view component çağrıldığında çalışır
+        {
+            return View(); // // View() metodu, ilgili view dosyasını döndürür
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListSizeFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListSizeFilterComponentPartial: ViewComponent // bu metot parçası, boyut filtresi için kullanılır
+    {
+        public IViewComponentResult Invoke() // Invoke metodu, bu view component çağrıldığında çalışır
+        {
+            return View(); // View() metodu, ilgili view dosyasını döndürür
+        }
+    }
+}
