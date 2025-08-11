336d27880ee0c8ad4778428d634d563c6715f100
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-11 21:16:15 +0300

    150# DTO Layer Added + Category List Process Add in Category Controller

---
+++ Frontends/MultiShop.DTOLayer/CatalogDTOs/CategoryDTOs/ResultCategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs
+{
+    public class ResultCategoryDTO // ResultCategoryDTO sınıfı, kategori sonuçlarını temsil eder.
+    {
+        public string CategoryID { get; set; } // Kategori ID'sini tuttuk. 
+        public string CategoryName { get; set; } // Kategori Adını Tuttuk.
+    }
+}
--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")] // Bu Area'nın adı "Admin" olarak ayarlanır. yani URL'de /Admin/ ile başlayacak.
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
-            ViewBag.v1="Ana Sayfa";
-            ViewBag.v2="Kategoriler";
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            ViewBag.v1="Ana Sayfa"; 
+            ViewBag.v2="Kategoriler"; 
             ViewBag.v3="Kategori Listesi";
             ViewBag.v4="Kategori İşlemleri";
+            // Bu ViewBag'ler, view içinde kullanılacak verileri taşır.
+
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage=await client.GetAsync("https://localhost:1002/api/Categories"); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values= JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
             return View();
         }
     }
