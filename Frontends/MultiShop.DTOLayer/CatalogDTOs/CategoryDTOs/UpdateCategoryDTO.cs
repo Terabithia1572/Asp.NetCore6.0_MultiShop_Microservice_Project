516ce798e8ccd27a4b5eee5a80bfef219a56255c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-13 20:18:49 +0300

    157# Fetching Category Data to be Updated

---
+++ Frontends/MultiShop.DTOLayer/CatalogDTOs/CategoryDTOs/UpdateCategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs
+{
+    public class UpdateCategoryDTO
+    {
+        public string CategoryID { get; set; } // Kategori ID'sini tuttuk. 
+        public string CategoryName { get; set; } // Kategori Adını Tuttuk.
+    }
+}
