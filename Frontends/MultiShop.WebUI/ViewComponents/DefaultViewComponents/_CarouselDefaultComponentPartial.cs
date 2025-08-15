fc1d1fc10e18fba79c54a5d9a69dda5f229a8015
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-15 21:29:49 +0300

    173# Retrieving Images from MongoDB in Feature Slider Area Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.FeatureSliderDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CarouselDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() //bu metod, bu view component çağrıldığında çalışır
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarouselDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, bu view component için varsayılan view'ı döndürür.
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() //bu metod, bu view component çağrıldığında çalışır
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/FeatureSliders"); // API'den önce çıkan özellik verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
         }
     }
 }
