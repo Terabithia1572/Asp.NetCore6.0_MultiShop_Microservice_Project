e85e6ac6f1bceeae9b6f97f7cdfc7deaf8564f70
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-16 19:47:02 +0300

    182# Feature Web UI Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.FeatureDTOs;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() //bu metot, bu view component çağrıldığında çalışır.
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, bu view component için varsayılan view dosyasını döndürür.
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task< IViewComponentResult> InvokeAsync() //bu metot, bu view component çağrıldığında çalışır.
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Önce Çıkan Özellikler";
+            ViewBag.v3 = "Önce Çıkan Özellik Listesi";
+            ViewBag.v4 = "Önce Çıkan Özellik İşlemleri";
+            // Bu ViewBag'ler, view içinde kullanılacak verileri taşır.
+
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/Features"); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
         }
     }
 }
