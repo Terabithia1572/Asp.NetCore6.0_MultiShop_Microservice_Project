139f6b79705e277a95d1c9c2f1d2bb3a6eeb44d5
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-16 15:56:53 +0300

    177# SpecialOffer API CRUD Process Added

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.SpecialOfferDTOs;
+using MultiShop.Catalog.Services.SpecialOfferServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous] // Bu controller'a erişim için yetkilendirme gereklidir.
+    [Route("api/[controller]")] // API'nin URL yolunu tanımlar, [controller] kısmı bu controller'ın adını alır.
+    [ApiController] // Bu sınıfın bir API controller olduğunu belirtir, bu sayede otomatik olarak model doğrulama ve hata işleme gibi özellikler eklenir.
+    public class SpecialOffersController : ControllerBase
+    {
+        private readonly ISpecialOfferService _specialOfferService; // SpecialOffer Service için Dependency Injection 
+
+        public SpecialOffersController(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService; // Constructor üzerinden SpecialOffer Service'i alıyoruz
+        }
+        [HttpGet]
+        public async Task<IActionResult> SpecialOfferList()
+        {
+            var values = await _specialOfferService.GetAllSpecialOfferAsync(); // SpecialOffer Service üzerinden tüm indirimleri alıyoruz
+            if (values == null || !values.Any())
+            {
+                return NotFound("İndirim Bulunamadı."); // Eğer indirim bulunamazsa 404 döndürüyoruz
+            }
+            return Ok(values); // İndirimler bulunduysa 200 OK ile birlikte indirimleri döndürüyoruz
+        }
+        [HttpGet("{id}")] // Belirli bir indirim için id parametresi alıyoruz 
+        public async Task<IActionResult> GetSpecialOfferByID(string id)
+        {
+            var value = await _specialOfferService.GetByIDSpecialOfferAsync(id); // SpecialOffer Service üzerinden id ile indirim alıyoruz
+            if (value == null)
+            {
+                return NotFound("İndirim Bulunamadı."); // Eğer indirim bulunamazsa 404 döndürüyoruz
+            }
+            return Ok(value); // İndirim bulunduysa 200 OK ile birlikte indirimyi döndürüyoruz
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDTO createSpecialOfferDTO)
+        {
+            await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDTO); // SpecialOffer Service üzerinden yeni indirim oluşturuyoruz
+            return Ok("İndirim Başarıyla Oluşturuldu."); // İndirim başarıyla oluşturulduysa 200 OK ile birlikte mesaj döndürüyoruz
+        }
+        [HttpDelete] // İndirim silme işlemi için
+        public async Task<IActionResult> DeleteSpecialOffer(string id)
+        {
+            await _specialOfferService.DeleteSpecialOfferAsync(id); // SpecialOffer Service üzerinden id ile indirim siliyoruz
+            return Ok("İndirim Başarıyla Silindi."); // İndirim başarıyla silindiyse 200 OK ile birlikte mesaj döndürüyoruz
+        }
+        [HttpPut] // İndirim güncelleme işlemi için
+        public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDTO updateSpecialOfferDTO)
+        {
+            await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDTO); // SpecialOffer Service üzerinden güncelleme işlemi yapıyoruz
+            return Ok("İndirim Başarıyla Güncellendi."); // İndirim başarıyla güncellendiyse 200 OK ile birlikte mesaj döndürüyoruz
+        }
+    }
+}
--- Services/Catalog/MultiShop.Catalog/Entities/SpecialOffer.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/SpecialOffer.cs
@@ -1,7 +1,13 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class SpecialOffer
     {
+        [BsonId] //Bu attribute, hangi property’nin MongoDB’de “_id” alanı olarak tutulacağını belirtir.
+
+        [BsonRepresentation(BsonType.ObjectId)] // Bu attribute, MongoDB’deki ObjectId türündeki verilerin string olarak temsil edilmesini sağlar.
         public string SpecialOfferID { get; set; } //Özel Teklif ID
         public string SpecialOfferTitle { get; set; } //Özel Teklif Başlığı
         public string SpecialOfferSubTitle { get; set; } //Özel Teklif alt Başlığı
