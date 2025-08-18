4a1cb39f510e7c3fde3946c8c3b781cdc0ce6316
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-18 20:10:17 +0300

    186# Featured Products on the Home Page Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductsDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() //bu metot, bu view component çağrıldığında çalışır.
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, bu view component için varsayılan view dosyasını döndürür.
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/Products"); // API'den ürün verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View(); // Eğer istek başarısızsa, boş bir view döndürülür.
         }
     }
 }
