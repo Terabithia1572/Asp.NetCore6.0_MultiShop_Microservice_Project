64a0d8ee522ad9fa3eb3c018b6f382b3bedd26a1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-18 21:45:23 +0300

    194# Listing Brands in UI Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_VendorDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_VendorDefaultComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.BrandDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _VendorDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() //bu metot, bu view component çağrıldığında çalışır.
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _VendorDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, bu view component için varsayılan view dosyasını döndürür.
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task< IViewComponentResult> InvokeAsync() //bu metot, bu view component çağrıldığında çalışır.
+        {
+          
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/Brands"); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
         }
     }
 }
