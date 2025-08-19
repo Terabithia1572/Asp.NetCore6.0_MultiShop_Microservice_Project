36c364932a188f70fe4d7af53193fd947f6f3276
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-19 21:22:10 +0300

    198# Listing the About Me Section in UI Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.AboutDTOs;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() //bu metot, bu view component çağrıldığında çalışır.
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, bu view component için varsayılan view dosyasını döndürür.
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task< IViewComponentResult> InvokeAsync() //bu metot, bu view component çağrıldığında çalışır.
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/Abouts"); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
         }
     }
 }
