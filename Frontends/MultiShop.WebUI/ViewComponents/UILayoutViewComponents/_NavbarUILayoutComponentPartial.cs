50e884aeb14bd228c02d9bcfc9fd7eb568b04c33
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-20 21:41:40 +0300

    202# Listing Categories in Layout Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _NavbarUILayoutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() //bu metod, bu view component çağrıldığında çalışır
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _NavbarUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, bu view component için varsayılan view'ı döndürür.
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/Categories"); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
         }
     }
 }
