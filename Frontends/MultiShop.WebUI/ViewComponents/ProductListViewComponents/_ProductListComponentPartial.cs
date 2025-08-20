5f45f1efd80510ed4d146f9c04aba10e76d3e14a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-20 21:35:58 +0300

    201# Fetching Products Statically by Category Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
 {
     public class _ProductListComponentPartial: ViewComponent // bu sınıf ViewComponent sınıfından türetilir 
     {
-        public IViewComponentResult Invoke() // Invoke metodu, bu ViewComponent çağrıldığında çalışacak metottur.
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductListComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, varsayılan olarak _ProductListComponentPartial.cshtml dosyasını render eder.
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string id) //bu metot, bu view component çağrıldığında çalışır.
+        {
+
+            id = "68a0b8dfac2a8372b6bd8abc"; // Örnek olarak bir kategori ID'si atanır. Bu, gerçek uygulamada dinamik olarak değişebilir.
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/Products/ProductListWithCategoryByCategoryID?id="+id); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
         }
     }
 }
