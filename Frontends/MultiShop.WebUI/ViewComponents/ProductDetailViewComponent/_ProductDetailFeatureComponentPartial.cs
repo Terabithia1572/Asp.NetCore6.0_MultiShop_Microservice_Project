ebccfa5e160742f0840d16202286316851e0ce86
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-21 21:17:16 +0300

    205# Product Detail Page Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailFeatureComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailFeatureComponentPartial.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
 {
     public class _ProductDetailFeatureComponentPartial: ViewComponent // Bu sınıf, ürün detayları için bir görüntü bileşeni sağlar
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task< IViewComponentResult> InvokeAsync(string id)
         {
             // Burada, ürün detayları için gerekli verileri alabilir ve görüntüleyebilirsiniz.
             // Örneğin, bir model veya veri kaynağı kullanabilirsiniz.
             // Şu anda sadece örnek bir görünüm döndürüyoruz.
-            return View();
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync($"https://localhost:1002/api/Products/{id}"); // API'den kategori verisi alınır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<UpdateProductDTO>(jsonData); // JSON verisi DTO nesnesine dönüştürülür.
+                return View(values); // Dönüştürülen DTO nesnesi view'e gönderilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
         }
     }
 }
