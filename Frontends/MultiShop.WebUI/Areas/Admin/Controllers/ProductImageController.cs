b0def503c5206ebf62a457b7ead9deca04e1f165
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-22 20:48:06 +0300

    210# Product Image Update Consume Code Process Added

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
@@ -1,13 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductImageDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    [AllowAnonymous]
     public class ProductImageController : Controller
     {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductImageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("{id}")]
         public async Task<IActionResult> ProductImageDetail(string id)
         {
-
-            return View();
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün Görsel Güncelleme";
+            ViewBag.v4 = "Ürün İşlemleri";
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync($"https://localhost:1002/api/ProductImages/ProductImagesByProductID?id=/{id}"); // API'den kategori verisi alınır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<UpdateProductImageDTO>(jsonData); // JSON verisi DTO nesnesine dönüştürülür.
+                return View(values); // Dönüştürülen DTO nesnesi view'e gönderilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
+        }
+        [HttpPost("{id}")]
+        public async Task<IActionResult> ProductImageDetail(UpdateProductImageDTO updateProductImageDTO)
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var jsonData = JsonConvert.SerializeObject(updateProductImageDTO); // DTO nesnesi JSON formatına dönüştürülür.
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisi StringContent olarak hazırlanır.
+            var responseMessage = await client.PutAsync("https://localhost:1002/api/ProductImages/", content); // API'ye PUT isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                return RedirectToAction("ProductListWithCategory", "Product", new { area = "Admin" }); // Kategori listesine yönlendirilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
         }
     }
 }
