35e70c2add2a2a24ae7c249c987daddda8468318
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-24 17:13:34 +0300

    216# Listing Product Detail in UI Process Added

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailDescriptionComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailDescriptionComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDetailDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponent
 {
     public class _ProductDetailDescriptionComponentPartial: ViewComponent // bu sınıf ViewComponent sınıfından türetilmiştir
     {
-        public IViewComponentResult Invoke() // Invoke metodu, bu ViewComponent çağrıldığında çalışacak metottur
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ProductDetailDescriptionComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); // View() metodu, varsayılan olarak _ProductDetailDescriptionComponentPartial.cshtml dosyasını render eder
+            _httpClientFactory = httpClientFactory;
         }
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            ViewBag.v = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:1002/api/ProductDetails/GetProductDetailByProductID/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDetailDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
     }
 }
