34fdf7b0a79fb6d2bb92acbc7dc47dd0fd4cb282
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-25 20:50:19 +0300

    219# CommentDB Migration Process Added

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442;initial Catalog=MultiShopCommentDB;User=sa;Password=Yunus6565*");
+        }
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
---
+++ Services/Comment/MultiShop.Comment/Migrations/20250825174922_mig2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Comment.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserComments",
+                columns: table => new
+                {
+                    UserCommentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserCommentNameSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserCommentImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserCommentEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserCommentDetail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserCommentRating = table.Column<int>(type: "int", nullable: false),
+                    UserCommentCreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserCommentStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserComments", x => x.UserCommentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserComments");
+        }
+    }
+}
