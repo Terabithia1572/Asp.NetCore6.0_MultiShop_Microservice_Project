aac1dd789961102ac1b363c9f8649b2387ae9e7d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-25 21:36:46 +0300

    221# Admin Panel Comment List and the Importance of Docker Process Added

---
+++ Frontends/MultiShop.DTOLayer/CommentDTOs/ResultCommentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.CommentDTOs
+{
+    public class ResultCommentDTO
+    {
+        public int UserCommentID { get; set; } //Kullanıcı Yorum ID
+        public string UserCommentNameSurname { get; set; } //Kullanıcı Yorum Ad Soyad
+        public string? UserCommentImageURL { get; set; } //Kullanıcı Yorum Resim URL
+        public string UserCommentEmail { get; set; } //Kullanıcı Yorum Email
+        public string UserCommentDetail { get; set; } //Kullanıcı Yorum Detay
+        public int UserCommentRating { get; set; } //Kullanıcı Yorum Puan
+        public DateTime UserCommentCreatedDate { get; set; } //Kullanıcı Yorum Oluşturma Tarihi
+        public bool UserCommentStatus { get; set; } //Kullanıcı Yorum Durum
+        public string ProductID { get; set; } //Ürün ID
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CommentDTOs;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")] // Bu Area'nın adı "Admin" olarak ayarlanır. yani URL'de /Admin/ ile başlayacak.
+    [Route("Admin/[controller]/[action]")] // Bu controller için rota ayarlanır. Örneğin: /Admin/Comment/Index
+    public class CommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Yorumlar";
+            ViewBag.v3 = "Yorum Listesi";
+            ViewBag.v4 = "Yorum İşlemleri";
+            // Bu ViewBag'ler, view içinde kullanılacak verileri taşır.
+
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync("https://localhost:7297/api/Comments"); // API'den kategori verilerini almak için GET isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDTO>>(jsonData); // JSON verisi dinamik bir listeye dönüştürülür.
+                return View(values); // Dönüştürülen liste view'e gönderilir.
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateComment()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Yorumlar";
+            ViewBag.v3 = "Yeni Yorum Ekleme";
+            ViewBag.v4 = "Yorum İşlemleri";
+            return View(); // Yorum ekleme sayfası için view döndürülür.
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateComment(CreateCommentDTO createCommentDTO)
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var jsonData = JsonConvert.SerializeObject(createCommentDTO); // DTO nesnesi JSON formatına dönüştürülür.
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisi StringContent olarak hazırlanır.
+            var responseMessage = await client.PostAsync("https://localhost:7297/api/Comments", content); // API'ye POST isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" }); // Yorum listesine yönlendirilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> DeleteComment(string id)
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.DeleteAsync($"https://localhost:7297/api/Comments?id=" + id); // API'den kategori silme isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" }); // Yorum listesine yönlendirilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> UpdateComment(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Yorumlar";
+            ViewBag.v3 = "Yorum Güncelleme";
+            ViewBag.v4 = "Yorum İşlemleri";
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var responseMessage = await client.GetAsync($"https://localhost:7297/api/Comments/{id}"); // API'den kategori verisi alınır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // JSON verisi okunur.
+                var values = JsonConvert.DeserializeObject<UpdateCommentDTO>(jsonData); // JSON verisi DTO nesnesine dönüştürülür.
+                return View(values); // Dönüştürülen DTO nesnesi view'e gönderilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
+        }
+        [HttpPost("{id}")]
+        public async Task<IActionResult> UpdateComment(UpdateCommentDTO updateCommentDTO)
+        {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var jsonData = JsonConvert.SerializeObject(updateCommentDTO); // DTO nesnesi JSON formatına dönüştürülür.
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisi StringContent olarak hazırlanır.
+            var responseMessage = await client.PutAsync("https://localhost:7297/api/Comments/", content); // API'ye PUT isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" }); // Yorum listesine yönlendirilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
+        }
+    }
+}
