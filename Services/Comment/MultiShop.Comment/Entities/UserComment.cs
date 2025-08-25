d61ce10038fab4a3269cc57c965b3425c2dc5771
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-25 20:37:56 +0300

    218# Comment MicroService Added

---
+++ Services/Comment/MultiShop.Comment/Entities/UserComment.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MultiShop.Comment.Entities
+{
+    public class UserComment
+    {
+        public int UserCommentID { get; set; } //Kullanıcı Yorum ID
+        public string UserCommentNameSurname { get; set; } //Kullanıcı Yorum Ad Soyad
+        public string? UserCommentImageURL { get; set; } //Kullanıcı Yorum Resim URL
+        public string UserCommentEmail { get; set; } //Kullanıcı Yorum Email
+        public string UserCommentDetail { get; set; } //Kullanıcı Yorum Detay
+        public int UserCommentRating { get; set; } //Kullanıcı Yorum Puan
+        public DateTime UserCommentCreatedDate { get; set; } //Kullanıcı Yorum Oluşturma Tarihi
+        public bool UserCommentStatus { get; set; } //Kullanıcı Yorum Durum
+    }
+}
