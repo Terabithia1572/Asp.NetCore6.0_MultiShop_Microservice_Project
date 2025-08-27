5d0e400ef7a7006152688dc95d4bc5d3d0c3c8a1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-27 21:46:06 +0300

    235# Register Process Added

---
+++ Frontends/MultiShop.DTOLayer/IdentityDTOs/RegisterDTOs/CreateRegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.IdentityDTOs.RegisterDTOs
+{
+    public class CreateRegisterDTO
+    {
+        public string Username { get; set; } // Kullanıcı adı
+        public string Password { get; set; } // Şifre
+        public string ConfirmPassword { get; set; } // Şifre
+        public string Email { get; set; } // E-posta adresi
+        public string Name { get; set; } // Ad
+        public string Surname { get; set; } // Soyad
+    }
+}
--- Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
+++ Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.IdentityDTOs.RegisterDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateRegisterDTO createRegisterDTO)
+        {
+            if(createRegisterDTO.Password==createRegisterDTO.ConfirmPassword) // Şifre ve onay şifresi eşleşmiyorsa
+            {
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var jsonData = JsonConvert.SerializeObject(createRegisterDTO); // DTO nesnesi JSON formatına dönüştürülür.
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisi StringContent olarak hazırlanır.
+            var responseMessage = await client.PostAsync("http://localhost:5001/api/Registers", content); // API'ye POST isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                return RedirectToAction("Index", "Login"); // Giriş kısmına yönlendirilir.
+            }
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
         }
     }
 }
