3b1d93b8d4f7ad86f11d2dff637cc1e07f16d1a3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-27 20:33:09 +0300

    232# Send Message CRUD Process Added

---
+++ Frontends/MultiShop.DTOLayer/CatalogDTOs/ContactDTOs/GetByIDContactDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.CatalogDTOs.ContactDTOs
+{
+    public class GetByIDContactDTO
+    {
+        public string ContactID { get; set; } // MongoDB'de benzersiz kimlik olarak kullanılacak
+        public string ContactNameSurname { get; set; } //İletişim Adını Tuttuk.
+        public string ContactEmail { get; set; } //İletişim Resim Görselini Tuttuk.
+        public string ContactSubject { get; set; } //İletişim Konu Başlığını Tuttuk.
+        public string ContactMessage { get; set; } //İletişim Mesajını Tuttuk.
+        public DateTime ContactCreatedDate { get; set; } //İletişim Oluşturma Tarihini Tuttuk.
+        public bool ContactIsRead { get; set; } //İletişim Durumunu Tuttuk.
+    }
+}
--- Frontends/MultiShop.WebUI/Controllers/ContactController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ContactDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(); // Kategori ekleme sayfası için view döndürülür.
+        }
+        [HttpPost]
+        public async Task< IActionResult> Index(CreateContactDTO createContactDTO)
+        {
+            createContactDTO.ContactIsRead = false; // Yeni eklenen mesajın okunmadı olarak işaretlenmesi
+            createContactDTO.ContactCreatedDate = DateTime.Now; // Mesajın oluşturulma tarihinin şu an olarak ayarlanması
+            var client = _httpClientFactory.CreateClient(); // IHttpClientFactory kullanarak HttpClient oluşturulur.
+            var jsonData = JsonConvert.SerializeObject(createContactDTO); // DTO nesnesi JSON formatına dönüştürülür.
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisi StringContent olarak hazırlanır.
+            var responseMessage = await client.PostAsync("https://localhost:1002/api/Contacts", content); // API'ye POST isteği yapılır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer istek başarılıysa
+            {
+                return RedirectToAction("Index", "Default"); // Kategori listesine yönlendirilir.
+            }
+            return View(); // Başarısız ise aynı view döndürülür.
         }
     }
 }
