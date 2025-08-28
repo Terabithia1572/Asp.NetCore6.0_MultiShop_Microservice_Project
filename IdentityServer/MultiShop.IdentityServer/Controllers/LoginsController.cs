0a276ad3b9eef56d2d69c6c161ba59f042e9a0a1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-28 21:53:04 +0300

    247# Login Process Added

--- IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.IdentityServer.DTOs;
 using MultiShop.IdentityServer.Models;
+using MultiShop.IdentityServer.Tools;
 using System.Threading.Tasks;
 
 namespace MultiShop.IdentityServer.Controllers
@@ -28,7 +29,12 @@ namespace MultiShop.IdentityServer.Controllers
             }
             else
             {
-                return Ok("Giriş işlemi başarılı.");
+                GetCheckAppUserViewModel getCheckAppUserViewModel = new GetCheckAppUserViewModel();
+                getCheckAppUserViewModel.UserName= userLoginDTO.Username;
+                getCheckAppUserViewModel.ID= "1"; //Kullanıcı ID'si burada atanmalı
+                var token = JwtTokenGenerator.GenerateToken(getCheckAppUserViewModel);
+
+                return Ok(token);
 
             }
         }
