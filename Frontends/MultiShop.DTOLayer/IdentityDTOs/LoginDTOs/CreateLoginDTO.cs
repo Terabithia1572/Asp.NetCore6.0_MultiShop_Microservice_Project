6ba66da9e1d35a2f5552034908953f470706d15b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-28 20:48:12 +0300

    240# Backend Codes for Login Process in UI Process Added

---
+++ Frontends/MultiShop.DTOLayer/IdentityDTOs/LoginDTOs/CreateLoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.IdentityDTOs.LoginDTOs
+{
+    public class CreateLoginDTO
+    {
+        public string UserName { get; set; } // Kullanıcı adı
+        public string Password { get; set; } // Şifre
+    }
+}
