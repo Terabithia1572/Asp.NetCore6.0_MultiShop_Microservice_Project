3fda828fa904ffb441459df082536b283e624a84
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-30 16:11:59 +0300

    257# Discovery End Point Process Added

--- Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
+++ Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -1,13 +1,39 @@
-﻿using MultiShop.DTOLayer.IdentityDTOs.LoginDTOs;
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using MultiShop.DTOLayer.IdentityDTOs.LoginDTOs;
 using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Settings;
 
 namespace MultiShop.WebUI.Services.Concrete
 {
     public class IdentityService : IIdentityService
     {
-        public Task<bool> SignIn(SignUpDTO signUpDTO)
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClientSettings _clientSettings;
+        public IdentityService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IOptions<ClientSettings> clientSettings)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+            _clientSettings = clientSettings.Value;
+        }
+
+        public async Task<bool> SignIn(SignUpDTO signUpDTO)
+        {
+            var discoveryEndpoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = "https://localhost:5001",
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            });
+
+            var passwordTokenRequest = new PasswordTokenRequest
+            {
+                ClientId = _clientSettings.MultiShopManagerID.ClientID,
+                ClientSecret = _clientSettings.MultiShopManagerID.ClientSecret,
+                UserName = signUpDTO.UserName,
+                Password = signUpDTO.Password,
+                Address = discoveryEndpoint.TokenEndpoint,
+            };
         }
     }
 }
