3b3351cced149f912edb256437b1dcef5cc41849
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-08-30 14:54:12 +0300

    256# Client Setting Class Process Added

---
+++ Frontends/MultiShop.WebUI/Settings/ClientSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.WebUI.Settings
+{
+    public class ClientSettings
+    {
+        public Client MultiShopVisitorClient { get; set; } // Ziyaretçi istemcisi için kimlik bilgileri
+        public Client MultiShopManagerID { get; set; } // Yönetici istemcisi için kimlik bilgileri
+        public Client MultiShopAdminID { get; set; } // Admin istemcisi için kimlik bilgileri
+    }
+    public class Client
+    {
+        public string ClientID { get; set; } // İstemci kimliği
+        public string ClientSecret { get; set; } // İstemci güvenlik anahtarı
+    }
+}
