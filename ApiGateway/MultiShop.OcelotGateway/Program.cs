9dcdc85d303904e6a652caf03d8b06624e1ec703
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-03 21:36:42 +0300

    271# Testing Catalog Microservices with Ocelot

--- ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -1,5 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+IConfiguration configuration=new ConfigurationBuilder().AddJsonFile("ocelot.json").Build(); // Bu kod 
+builder.Services.AddOcelot(configuration);
 var app = builder.Build();
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
