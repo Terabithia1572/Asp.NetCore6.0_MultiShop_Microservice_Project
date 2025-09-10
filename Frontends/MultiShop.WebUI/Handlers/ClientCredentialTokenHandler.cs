581d42216711c1577379322baeb20592ed7bd590
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-10 20:25:04 +0300

    290# Client Credential Token Handler Class Added

---
+++ Frontends/MultiShop.WebUI/Handlers/ClientCredentialTokenHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using MultiShop.WebUI.Services.Interfaces;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace MultiShop.WebUI.Handlers
+{
+    public class ClientCredentialTokenHandler:DelegatingHandler
+    {
+        private readonly IClientCredentialTokenService _clientCredentialTokenService; //Client Credential Token'ı alacak service
+
+        public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService) //Dependency Injection
+        {
+            _clientCredentialTokenService = clientCredentialTokenService; //Client Credential Token'ı alacak service
+        } 
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",await _clientCredentialTokenService.GetToken()); //Client Credential Token'ı request'in header'ına ekle
+            var response= await base.SendAsync(request, cancellationToken);
+            if(response.StatusCode==HttpStatusCode.Unauthorized) //Eğer response 401 dönerse
+            {
+                //hata yönetimi yapılabilir
+            }
+            return response; //Response'u döndür
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/Interfaces/IClientCredentialTokenService.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.WebUI.Services.Interfaces
+{
+    public interface IClientCredentialTokenService
+    {
+        Task<string> GetToken(); //Client Credential Token'ı alacak method
+    }
+}
