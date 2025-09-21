792d93a38351989118a0015015545bf98d71c59e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-21 19:48:36 +0300

    337# Sending Discount Code as Parameter Process Added

--- Frontends/MultiShop.WebUI/Services/DiscountServices/DiscountService.cs
+++ Frontends/MultiShop.WebUI/Services/DiscountServices/DiscountService.cs
@@ -13,7 +13,7 @@ namespace MultiShop.WebUI.Services.DiscountServices
 
         public async Task<GetDiscountCodeDetailByCode> GetDiscountCode(string couponCode)
         {
-           var responseMessage=await _httpClient.GetAsync($"discounts/GetCodeDetailByCode/{couponCode}"); //Kupon koduna göre kupon detaylarını aldık.
+           var responseMessage=await _httpClient.GetAsync("http://localhost:1003/api/Discounts/GetCodeDetailByCodeAsync?couponCode="+couponCode); //Kupon koduna göre kupon detaylarını aldık.
             var values=await responseMessage.Content.ReadFromJsonAsync<GetDiscountCodeDetailByCode>(); //Aldığımız kupon detaylarını GetDiscountCodeDetailByCode DTO'suna map ettik.
             return values;
         }
