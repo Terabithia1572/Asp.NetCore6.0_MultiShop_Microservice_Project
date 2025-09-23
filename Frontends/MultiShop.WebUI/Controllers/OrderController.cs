9b793cb00bf51ca295340a98c90f97e8f825eccb
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-23 20:14:48 +0300

    347# Order Transactions Entry

---
+++ Frontends/MultiShop.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Sipariş İşlemleri";
+            ViewBag.directory3 = "Siparişler";
+            return View();
+        }
+    }
+}
