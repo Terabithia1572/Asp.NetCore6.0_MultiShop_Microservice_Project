abe33920901297508ba83f7bd7bdea6330eca89f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-25 21:55:21 +0300

    360# Completing the Card Animation

--- Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
+++ Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -6,6 +6,9 @@ namespace MultiShop.WebUI.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.directory1 = "Ana Sayfa"; // Breadcrumb için
+            ViewBag.directory2 = "Ödeme İşlemleri"; // Breadcrumb için 
+            ViewBag.directory3 = "Kartla Ödeme";  // Breadcrumb için
             return View();
         }
     }
