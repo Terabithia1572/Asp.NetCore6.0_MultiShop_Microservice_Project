94187e03df1b9c60fefdc7a42ae999423d4c2071
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-26 22:00:32 +0300

    326# User Area Process Added

---
+++ Frontends/MultiShop.WebUI/Areas/User/Controllers/MyOrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.User.Controllers
+{
+    [Area("User")] // User alanını belirtir
+    public class MyOrderController : Controller
+    {
+        public IActionResult MyOrderList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Areas/User/Controllers/UserLayoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.User.Controllers
+{
+    [Area("User")] // User alanını belirtir
+    public class UserLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
