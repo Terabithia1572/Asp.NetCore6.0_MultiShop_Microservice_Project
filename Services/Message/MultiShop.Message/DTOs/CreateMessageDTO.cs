7394a8b377787587624eedb6ddf8b3cc260964bb
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-28 19:41:07 +0300

    383# Message DTO & Interface Services Process Added

---
+++ Services/Message/MultiShop.Message/DTOs/CreateMessageDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Message.DTOs
+{
+    public class CreateMessageDTO
+    {
+        public string MessageSenderID { get; set; } //Mesaj Gönderen Kullanıcı ID'si
+        public string MessageReceiverID { get; set; } //Mesaj Alan Kullanıcı ID'si
+        public string MessageSubject { get; set; } //Mesaj Konusu
+        public string MessageDetail { get; set; } //Mesaj Detayı 
+        public DateTime MessageDate { get; set; } //Mesajın Gönderildiği Tarih
+        public bool MessageStatus { get; set; } //Mesajın Okunma Durumu (Okundu/Okunmadı)
+    }
+}
---
+++ Services/Message/MultiShop.Message/Services/IUserMessageService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Message.DTOs;
+
+namespace MultiShop.Message.Services
+{
+    public interface IUserMessageService
+    {
+        Task<List<ResultMessageDTO>> GetAllMessageAsync(); //Veritabanındaki tüm mesajları listeleyen metot
+        Task<List<ResultInboxMessageDTO>> GetInboxMessageAsync(string messageID); //Gelen kutusundaki mesajları listeleyen metot
+        Task<List<ResultSendboxMessageDTO>> GetSendboxMessageAsync(string messageID); //Giden kutusundaki mesajları listeleyen metot
+        Task CreateMessageAsync(CreateMessageDTO createmessageDTO); //Yeni mesaj ekleyen metot
+        Task UpdateMessageAsync(UpdateMessageDTO updatemessageDTO); //Mevcut mesajı güncelleyen metot
+        Task DeleteMessageAsync(int messageID); //Mesajı silen metot
+        Task<GetByIDMessageDTO> GetByIDMessageAsync(int messageID); //ID'ye göre mesajı getiren metot
+
+    }
+}
