6cad420348cea6fbd3ea2cebb06648b8cda79201
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-28 20:59:16 +0300

    386# Testing API Process Added

---
+++ Services/Message/MultiShop.Message/Controllers/UserMessagesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Message.DTOs;
+using MultiShop.Message.Services;
+
+namespace MultiShop.Message.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserMessagesController : ControllerBase
+    {
+        private readonly IUserMessageService _userMessageService;
+
+        public UserMessagesController(IUserMessageService userMessageService)
+        {
+            _userMessageService = userMessageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMessage()
+        {
+            var values = await _userMessageService.GetAllMessageAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateMessageAsync(CreateMessageDTO createMessageDTO)
+        {
+            await _userMessageService.CreateMessageAsync(createMessageDTO);
+            return Ok("Mesaj Başarıyla Eklendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIDMessageAsync(int id)
+        {
+            var values = await _userMessageService.GetByIDMessageAsync(id);
+            return Ok(values);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessageAsync(int id)
+        {
+            await _userMessageService.DeleteMessageAsync(id);
+            return Ok("Mesaj Başarıyla Silindi");
+        }
+        [HttpGet("inbox/{id}")]
+        public async Task<IActionResult> GetInboxMessageAsync(string id)
+        {
+            var values = await _userMessageService.GetInboxMessageAsync(id);
+            return Ok(values);
+        }
+        [HttpGet("sendbox/{id}")]
+        public async Task<IActionResult> GetSendboxMessageAsync(string id)
+        {
+            var values = await _userMessageService.GetSendboxMessageAsync(id);
+            return Ok(values);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateMessageAsync(UpdateMessageDTO updateMessageDTO)
+        {
+            await _userMessageService.UpdateMessageAsync(updateMessageDTO);
+            return Ok("Mesaj Başarıyla Güncellendi");
+        }
+    }
+}
--- Services/Message/MultiShop.Message/Program.cs
+++ Services/Message/MultiShop.Message/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MultiShop.Message.DAL.Context;
+using MultiShop.Message.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEntityFrameworkNpgsql()
     .AddDbContext<MessageContext>(options =>
         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))); // Baðlantý stringi appsettings.json dosyasýndan alýnýyor
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // Uygulamadaki tüm AutoMapper profillerini yükler ve otomatik eþleme (mapping) iþlemlerinin kullanýlmasýný saðlar.
+builder.Services.AddScoped<IUserMessageService, UserMessageService>(); // IUserMessageService arayüzünün UserMessageService sýnýfý ile eþlenmesini saðlar. Bu sayede baðýmlýlýk enjeksiyonu (dependency injection) ile IUserMessageService türünde bir nesne talep edildiðinde, UserMessageService örneði saðlanýr. Scoped yaþam süresi, her HTTP isteði için tek bir örnek oluþturulmasýný garanti eder.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
