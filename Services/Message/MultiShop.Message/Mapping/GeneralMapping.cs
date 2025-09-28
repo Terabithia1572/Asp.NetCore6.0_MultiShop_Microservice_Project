91b3554ce474422be4c7cd92507c87c98e3f8ae7
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-28 19:49:05 +0300

    384# UserMessage & Auto Mapper Process Added

---
+++ Services/Message/MultiShop.Message/Mapping/GeneralMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MultiShop.Message.DAL.Entites;
+using MultiShop.Message.DTOs;
+
+namespace MultiShop.Message.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<UserMessage,ResultMessageDTO>().ReverseMap(); //UserMessage ile ResultMessageDTO arasında çift yönlü eşleme
+            CreateMap<UserMessage,CreateMessageDTO>().ReverseMap(); //UserMessage ile CreateMessageDTO arasında çift yönlü eşleme
+            CreateMap<UserMessage,UpdateMessageDTO>().ReverseMap(); //UserMessage ile UpdateMessageDTO arasında çift yönlü eşleme
+            CreateMap<UserMessage,GetByIDMessageDTO>().ReverseMap(); //UserMessage ile GetByIDMessageDTO arasında çift yönlü eşleme
+            CreateMap<UserMessage,ResultInboxMessageDTO>().ReverseMap(); //UserMessage ile ResultInboxMessageDTO arasında çift yönlü eşleme
+            CreateMap<UserMessage,ResultSendboxMessageDTO>().ReverseMap(); //UserMessage ile ResultSendboxMessageDTO arasında çift yönlü eşleme
+
+        }
+    }
+}
--- Services/Message/MultiShop.Message/Services/UserMessageService.cs
+++ Services/Message/MultiShop.Message/Services/UserMessageService.cs
@@ -1,6 +1,57 @@
-﻿namespace MultiShop.Message.Services
+﻿using AutoMapper;
+using MultiShop.Message.DAL.Context;
+using MultiShop.Message.DTOs;
+
+namespace MultiShop.Message.Services
 {
-    public class UserMessageService
+    public class UserMessageService : IUserMessageService
     {
+        private readonly MessageContext _messageContext;
+        private readonly IMapper _mapper;
+
+        public UserMessageService(MessageContext messageContext, IMapper mapper)
+        {
+            _messageContext = messageContext;
+            _mapper = mapper;
+        }
+
+        public async Task CreateMessageAsync(CreateMessageDTO createmessageDTO)
+        {
+            await _messageContext.AddAsync(createmessageDTO);
+        }
+
+        public async Task DeleteMessageAsync(int messageID)
+        {
+            var value = await _messageContext.FindAsync<GetByIDMessageDTO>(messageID);
+            if (value != null)
+            {
+                 _messageContext.Remove(value);
+            }
+        }
+
+        public Task<List<ResultMessageDTO>> GetAllMessageAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIDMessageDTO> GetByIDMessageAsync(int messageID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultInboxMessageDTO>> GetInboxMessageAsync(string messageID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultSendboxMessageDTO>> GetSendboxMessageAsync(string messageID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateMessageAsync(UpdateMessageDTO updatemessageDTO)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
