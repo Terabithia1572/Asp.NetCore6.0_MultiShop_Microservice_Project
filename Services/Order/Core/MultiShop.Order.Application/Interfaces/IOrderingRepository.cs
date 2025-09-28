0db37a489e83c21d740ef8a3fb751524d8e614eb
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-28 16:15:47 +0300

    369# Ordering Repository Process Added

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IOrderingRepository.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Order.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IOrderingRepository
+    {
+        public List<Ordering> GetOrderingsByUserID(string userID);
+    }
+}
---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Repositories/OrderingRepository.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entites;
+using MultiShop.Order.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Persistence.Repositories
+{
+    public class OrderingRepository : IOrderingRepository
+    {
+        private readonly OrderContext _orderContext;
+
+        public OrderingRepository(OrderContext orderContext)
+        {
+            _orderContext = orderContext;
+        }
+
+        public List<Ordering> GetOrderingsByUserID(string userID)
+        {
+           var values= _orderContext.Orderings.Where(o => o.OrderingUserID == userID).ToList();
+              return values;
+        }
+    }
+}
