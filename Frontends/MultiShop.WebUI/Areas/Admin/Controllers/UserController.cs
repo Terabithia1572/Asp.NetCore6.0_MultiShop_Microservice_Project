e46cfe8ed0a8c82817da0b158aff8fbf7afa94f4
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-29 23:15:25 +0300

    393# User List Process Added

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")] // Bu Area'nın Admin olduğunu belirtir
+    public class UserController : Controller
+    {
+        public async Task< IActionResult> UserList()
+        {
+            return View();
+        }
+    }
+}
