469680333c071b37247c32ea6c9a9eb06fc9f179
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-10-01 21:47:01 +0300

    404# Catalog Statistics Preparation Process Added

---
+++ Services/Catalog/MultiShop.Catalog/Services/StatisticServices/IStatisticService.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Catalog.Services.StatisticServices
+{
+    public interface IStatisticService
+    {
+        int GetCategoryCount(); // Bize Kategori Sayısını Verecek
+        int GetProduceCount(); // Ürün Sayısını vericek
+        int GetBrandsCount(); // Markaların sayısını vericek
+        decimal GetProductAvgPrice(); // Toplam Ürün Fiyatını verecek
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/StatisticServices/StatisticService.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.StatisticServices
+{
+    public class StatisticService : IStatisticService
+    {
+        private readonly IMongoCollection<Product> _productCollection; // MongoDB'deki Product koleksiyonuna erişim sağlar.
+        private readonly IMongoCollection<Category> _categoryCollection; // MongoDB'deki Category koleksiyonuna erişim sağlar.
+        private readonly IMongoCollection<Brand> _brandCollection; // MongoDB'deki Markalar koleksiyonuna erişim sağlar.
+
+        public StatisticService(IMongoCollection<Product> productCollection, IMongoCollection<Category> categoryCollection, IMongoCollection<Brand> brandCollection,IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString); // MongoDB ile bağlantı kurmak için istemci oluşturuluyor.
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); // Bağlantı kurulan MongoDB'de kullanılacak veritabanı seçiliyor.
+            _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName); // Product koleksiyonu üzerinden işlem yapabilmek için referans alınıyor.
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName); // Category koleksiyonu üzerinden işlem yapabilmek için referans alınıyor.
+            _brandCollection=database.GetCollection<Brand>(_databaseSettings.BrandCollectionName); // Brand koleksiyonu üzerinden işlem yapabilmek için referans alınıyor.
+        }
+
+        public int GetBrandsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetProduceCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetProductAvgPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
