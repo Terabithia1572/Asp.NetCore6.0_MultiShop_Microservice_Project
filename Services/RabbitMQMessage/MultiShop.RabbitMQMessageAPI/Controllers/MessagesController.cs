b1ffc6c445d9ba03a89abf555a8197aeb9c346ce
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-10-05 23:02:16 +0300

    453# Creating a Project for RabbitMQ

---
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageAPI/Controllers/MessagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.RabbitMQMessageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CreateMessage()
+        {
+            return Ok("Mesajınız Kuyruğa Alınmıştır..");
+        }
+    }
+}
