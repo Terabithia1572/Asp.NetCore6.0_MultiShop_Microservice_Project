b734d43e3207c633964bb3b9de71957919bce929
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-10-05 23:34:24 +0300

    457# Reading Messages with RabbitMQ Process Added

--- Services/RabbitMQMessage/MultiShop.RabbitMQMessageAPI/Controllers/MessagesController.cs
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageAPI/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace MultiShop.RabbitMQMessageAPI.Controllers
@@ -19,14 +20,37 @@ namespace MultiShop.RabbitMQMessageAPI.Controllers
             var connection = connectionFactory.CreateConnection();
 
             var channel = connection.CreateModel();
-            
-            channel.QueueDeclare("Kuyruk2",false,false,false,arguments:null);
+
+            channel.QueueDeclare("Kuyruk2", false, false, false, arguments: null);
 
             var messageContent = "Merhaba bugün hava çok soğuk..";
-            var byteMessageContent=Encoding.UTF8.GetBytes(messageContent);
-            channel.BasicPublish(exchange:"",routingKey:"Kuyruk2",basicProperties:null,body:byteMessageContent);
+            var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+            channel.BasicPublish(exchange: "", routingKey: "Kuyruk2", basicProperties: null, body: byteMessageContent);
 
             return Ok("Mesajınız Kuyruğa Alınmıştır..");
+        }
+        [HttpGet]
+        public IActionResult ReadMessage()
+        {
+            var connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName="localhost";
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            var consumer =new EventingBasicConsumer(channel);
+            string message="";
+            consumer.Received += (model, body) =>
+            {
+                var byteMessage = body.Body.ToArray();
+                 message = Encoding.UTF8.GetString(byteMessage);
+             
+            };
+
+            channel.BasicConsume(queue: "Kuyruk1", autoAck: false, consumer: consumer);
+
+
+            return Ok(message);
+
+           
         }
     }
 }
