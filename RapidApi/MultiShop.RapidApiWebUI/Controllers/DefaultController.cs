f83e2e53835d554de2ed372e7adfb9833a30a7ee
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-10-06 20:57:17 +0300

    462# Retrieving Dollar and Euro Exchange Rate Information via API

--- RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
@@ -29,5 +29,28 @@ namespace MultiShop.RapidApiWebUI.Controllers
                 return View();
             }    
         }
+        public async Task<IActionResult> Exchange()
+        {
+        
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/company-cash-flow?symbol=AAPL%3ANASDAQ&period=QUARTERLY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.Exchange=values.data.exchange_rate;
+                return View();
+            }
+        }
     }
 }
--- RapidApi/MultiShop.RapidApiWebUI/Program.cs
+++ RapidApi/MultiShop.RapidApiWebUI/Program.cs
@@ -1,4 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews(); //  en önemli satýr bu
+
+
 
 // Add services to the container.
 
@@ -19,6 +22,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+//  Bu satýr olmazsa MVC controller'larýn View'larý bulunmaz
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Default}/{action=WeatherDetail}/{id?}");
 
 app.MapControllers();
 
