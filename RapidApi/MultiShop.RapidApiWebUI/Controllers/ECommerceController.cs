4eb37923c292a65a4cb29c382ce5c5af0f9c6ad3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-10-06 21:11:44 +0300

    464# Fetching E-Commerce List

--- RapidApi/MultiShop.RapidApiWebUI/Controllers/ECommerceController.cs
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/ECommerceController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUI.Models;
+using Newtonsoft.Json;
 
 namespace MultiShop.RapidApiWebUI.Controllers
 {
@@ -6,7 +8,26 @@ namespace MultiShop.RapidApiWebUI.Controllers
     {
         public async Task< IActionResult> ECommerceList()
         {
-            return View();
+           
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-product-search.p.rapidapi.com/search-light-v2?q=Logitech%20f310&country=tr&language=en&page=1&limit=10&sort_by=BEST_MATCH&product_condition=ANY&return_filters=false"),
+                Headers =
+    {
+        { "x-rapidapi-key", "" },
+        { "x-rapidapi-host", "real-time-product-search.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ECommerceViewModel.Rootobject>>(body);
+                return View(values);
+            }
+           
         }
     }
 }
