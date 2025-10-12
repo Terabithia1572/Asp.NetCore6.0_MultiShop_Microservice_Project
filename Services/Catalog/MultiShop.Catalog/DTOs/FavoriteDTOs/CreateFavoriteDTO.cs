fcb8913943c0daba2cd3175944240a0ebf6461c5
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-10-12 17:20:56 +0300

    493# New Login Design Page Added

---
+++ Services/Catalog/MultiShop.Catalog/DTOs/FavoriteDTOs/CreateFavoriteDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Catalog.DTOs.FavoriteDTOs
+{
+    public class CreateFavoriteDTO
+    {
+        public string ProductID { get; set; }
+        public string UserID { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/DTOs/FavoriteDTOs/ResultFavoriteDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.DTOs.FavoriteDTOs
+{
+    public class ResultFavoriteDTO
+    {
+        public string FavoriteID { get; set; }
+        public string ProductID { get; set; }
+        public string UserID { get; set; }
+    }
+}
