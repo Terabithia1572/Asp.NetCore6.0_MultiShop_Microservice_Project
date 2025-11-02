dfba62a055c9316fd9a70503d46d577458b82c4f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-02 16:35:37 +0300

    540# Added Premium MultiShop Style Settings Page (Profile + App + Security Tabs, SweetAlert2, Preview, Gold Theme)

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.UserIdentityServices;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class SettingController : Controller
+    {
+        private readonly IUserIdentityService _userIdentityService;
+
+        public SettingController(IUserIdentityService userIdentityService)
+        {
+            _userIdentityService = userIdentityService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // 🔹 Giriş yapan kullanıcıyı (örneğin ID = cookie / token’dan)
+            // geçici olarak ilk kullanıcıyı çekelim
+            var users = await _userIdentityService.GetAllUserListAsync();
+            var currentUser = users.FirstOrDefault();
+
+            return View(currentUser);
+        }
+    }
+}
